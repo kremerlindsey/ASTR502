7065ff9a2e10e70dc9fde81ad9a35811b01793af
kremerlindsey <kremerlindsey05@gmail.com>
2026-04-20 15:22:00 -0400

    Add files via upload

--- crossmatching.py
+++ crossmatching.py
@@ -6,70 +6,70 @@ mega_list = pd.read_csv('/Users/lindseykremer/Downloads/ASTR502_Mega_Target_List
 
 ### Kounkel ###
 
-# mega_list['ticid'] = mega_list['tic_id'].str.replace('TIC', '', regex=False).astype('Int64')    # replace the letters in the mega target list's cells with empty space so they can be read as integers                                                                                                # the first in parentheses is what you want removed  
+mega_list['ticid'] = mega_list['tic_id'].str.replace('TIC', '', regex=False).astype('Int64')    # replace the letters in the mega target list's cells with empty space so they can be read as integers                                                                                                # the first in parentheses is what you want removed  
 
-# kounkel = Table.read('https://content.cld.iop.org/journals/1538-3881/164/4/137/revision1/ajac866dt1_mrt.txt', format = 'ascii.cds').to_pandas()
-#                                                                                                 # read in the source using its link    
-# print('catalogue read in done')
-# print(kounkel.columns)
-# print(mega_list.columns)
+kounkel = Table.read('https://content.cld.iop.org/journals/1538-3881/164/4/137/revision1/ajac866dt1_mrt.txt', format = 'ascii.cds').to_pandas()
+                                                                                                # read in the source using its link    
+print('catalogue read in done')
+print(kounkel.columns)
+print(mega_list.columns)
 
-# mega_with_kounkel = mega_list.merge(kounkel[['TIC', 'Period']], 
-#                                     left_on='ticid', 
-#                                     right_on='TIC').drop_duplicates(subset='tic_id')
+mega_with_kounkel = mega_list.merge(kounkel[['TIC', 'Period']], 
+                                    left_on='ticid', 
+                                    right_on='TIC').drop_duplicates(subset='tic_id')
 
-# print("Kounkel =", len(mega_with_kounkel), "matches")
+print("Kounkel =", len(mega_with_kounkel), "matches")
 
 ### Fetherolf ###
 
-# mega_list['ticid'] = mega_list['tic_id'].str.replace('TIC', '', regex=False).astype('Int64')    # replace the letters in the mega target list's cells with empty space so they can be read as integers
-#                                                                                                 # the first in parentheses is what you want removed  
+mega_list['ticid'] = mega_list['tic_id'].str.replace('TIC', '', regex=False).astype('Int64')    # replace the letters in the mega target list's cells with empty space so they can be read as integers
+                                                                                                # the first in parentheses is what you want removed  
 
-# fetherholf = Table.read('https://content.cld.iop.org/journals/0067-0049/268/1/4/revision1/apjsacdee5t1_mrt.txt', format = 'ascii.cds').to_pandas()
-#                                                                                                 # read in the source using its link    
-# print('catalogue read in done')
-# print(fetherholf.columns)
-# print(mega_list.columns)
+fetherholf = Table.read('https://content.cld.iop.org/journals/0067-0049/268/1/4/revision1/apjsacdee5t1_mrt.txt', format = 'ascii.cds').to_pandas()
+                                                                                                # read in the source using its link    
+print('catalogue read in done')
+print(fetherholf.columns)
+print(mega_list.columns)
 
-# mega_with_fetherholf = mega_list.merge(fetherholf[['TIC', 'P']], 
-#                                     left_on='ticid', 
-#                                     right_on='TIC').drop_duplicates(subset='tic_id')
+mega_with_fetherholf = mega_list.merge(fetherholf[['TIC', 'P']], 
+                                    left_on='ticid', 
+                                    right_on='TIC').drop_duplicates(subset='tic_id')
 
-# print("Fetherholf =", len(mega_with_fetherholf), "matches")
+print("Fetherholf =", len(mega_with_fetherholf), "matches")
 
 ### Lu ###
 
-# mega_list['gaiadr3'] = mega_list['gaia_dr3_id'].str.replace('Gaia DR3', '', regex=False).astype('Int64')    # replace the letters in the mega target list's cells with empty space so they can be read as integers
-#                                                                                                 # the first in parentheses is what you want removed  
+mega_list['gaiadr3'] = mega_list['gaia_dr3_id'].str.replace('Gaia DR3', '', regex=False).astype('Int64')    # replace the letters in the mega target list's cells with empty space so they can be read as integers
+                                                                                                # the first in parentheses is what you want removed  
 
-# lu = Table.read('https://content.cld.iop.org/journals/1538-3881/164/6/251/revision1/ajac9beet1_mrt.txt', format = 'ascii.cds').to_pandas()
-#                                                                                                 # read in the source using its link    
-# print('catalogue read in done')
-# print(lu.columns)
-# print(mega_list.columns)
+lu = Table.read('https://content.cld.iop.org/journals/1538-3881/164/6/251/revision1/ajac9beet1_mrt.txt', format = 'ascii.cds').to_pandas()
+                                                                                                # read in the source using its link    
+print('catalogue read in done')
+print(lu.columns)
+print(mega_list.columns)
 
-# mega_with_lu = mega_list.merge(lu[['EDR3', 'Prot']], 
-#                                     left_on='gaiadr3', 
-#                                     right_on='EDR3').drop_duplicates(subset='gaia_dr3_id')
+mega_with_lu = mega_list.merge(lu[['EDR3', 'Prot']], 
+                                    left_on='gaiadr3', 
+                                    right_on='EDR3').drop_duplicates(subset='gaia_dr3_id')
 
-# print("Lu =", len(mega_with_lu), "matches")
+print("Lu =", len(mega_with_lu), "matches")
 
 ### Holcomb ###
 
-# mega_list['ticid'] = mega_list['tic_id'].str.replace('TIC', '', regex=False).astype('Int64')    # replace the letters in the mega target list's cells with empty space so they can be read as integers
-#                                                                                                 # the first in parentheses is what you want removed  
+mega_list['ticid'] = mega_list['tic_id'].str.replace('TIC', '', regex=False).astype('Int64')    # replace the letters in the mega target list's cells with empty space so they can be read as integers
+                                                                                                # the first in parentheses is what you want removed  
 
-# holcomb = Table.read('/Users/lindseykremer/Downloads/spinspotter_by_star.csv').to_pandas()
-#                                                                                                 # read in the source using its link    
-# print('catalogue read in done')
-# print(holcomb.columns)
-# print(mega_list.columns)
+holcomb = Table.read('/Users/lindseykremer/Downloads/spinspotter_by_star.csv').to_pandas()
+                                                                                                # read in the source using its link    
+print('catalogue read in done')
+print(holcomb.columns)
+print(mega_list.columns)
 
-# mega_with_holcomb = mega_list.merge(holcomb[['TICID', 'Prot']], 
-#                                     left_on='ticid', 
-#                                     right_on='TICID').drop_duplicates(subset='tic_id')
+mega_with_holcomb = mega_list.merge(holcomb[['TICID', 'Prot']], 
+                                    left_on='ticid', 
+                                    right_on='TICID').drop_duplicates(subset='tic_id')
 
-# print("Holcomb =", len(mega_with_holcomb), "matches")
+print("Holcomb =", len(mega_with_holcomb), "matches")
 
 ### Colman ###
 
